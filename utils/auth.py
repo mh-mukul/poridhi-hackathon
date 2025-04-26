585d17855eb9a8c917d1bc06173011044ab51f77
mukulmh <mukulseu@gmail.com>
2025-04-26 15:42:20 +0600

    static api key auth added

--- utils/auth.py
+++ utils/auth.py
@@ -1,30 +1,26 @@
-from sqlalchemy.orm import Session
+from dotenv import load_dotenv
+import os
 from fastapi import Depends, Security
 from fastapi.security.api_key import APIKeyHeader
 from handlers.exception_handler import APIKeyException
 
-from models import ApiKey
-from configs.database import get_db
-
 api_key_header = APIKeyHeader(name="Authorization", auto_error=False)
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
 
 
 async def get_api_key(
     api_key: str = Security(api_key_header),
-    db: Session = Depends(get_db)
 ):
     if api_key is None:
         raise APIKeyException(
             status=401, message="Authorization header missing")
 
     token = api_key.replace("Bearer ", "")
-    api_key_obj = db.query(ApiKey).filter(
-        ApiKey.key == token,
-        ApiKey.is_active == True,
-        ApiKey.is_deleted == False
-    ).first()
 
-    if not api_key_obj:
+    if not token == API_KEY:
         raise APIKeyException(status=403, message="Invalid API Key")
 
-    return api_key_obj
+    return True
